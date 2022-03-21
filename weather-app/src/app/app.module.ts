31a9e3b7068f04783cef195fe9f431a0edfe98b3
Ganesh kumbham <kganeshkumbham@gmail.com>
2022-03-21 21:36:39 +0530

    weather-application

---
+++ weather-app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ForecastComponent } from './forecast/forecast.component';
+import { LandingScreenComponent } from './landing-screen/landing-screen.component';
+
+@NgModule({
+  declarations: [AppComponent, ForecastComponent, LandingScreenComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
